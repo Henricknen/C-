962c360d9e21a491ec90a297de25d9800c2a921e
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-02-25 19:38:34 -0300

    criando_estrutura_basica_de_um_codigo_em_C++

---
+++ Estrutura_basica.cpp
@@ -0,0 +1,12 @@
+#include <iostream>    // '#include' faz a inclusão de bibliotecas biblioteca 'iostream' é de comandos basicos de escrita, de entrada e saida
+
+using namespace std;        // definição do 'namespace' std, que é o espaço de trabalho que onde será busacado as informações
+
+int main() {        // 'corpo' função principal chamada 'main'
+
+    cout <<"Repositorio de C++\nPrimeiros passos para codificacao C++ !" << endl;        // 'cout' imprime todo conteudo dentro das "" e '\n' e 'endl' da uma quebra de linha
+
+    system("pause");        // ultilizando uma 'pausa' para prar o programa
+    return 0;       // 'return' indica o que está função vai retornar para o sistema
+
+}
