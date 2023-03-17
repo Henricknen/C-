60ed1b41086b26669c2b6fe14b8eb8fca5f7554a
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-03-17 16:57:12 -0300

    fazendo_conversao_de_base_em_'cout'

---
+++ printf_cout.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdio.h>
+#include <math.h>       // incluindo biblioteca 'math.h' biblioteca matemática
+
+using namespace std;
+int main() {
+
+    int num = 15;
+
+    //printf("Valor de NUM %07d", num);       // %07d '07' indica o que estará sendo completado com seis 0 a esquerda
+    cout << "Valor de NUM em dec - " << num << "\n";
+    cout << "Valor de NUM em hexa - " << hex << num << "\n";      // 'hex' converte a variável em hexadeçimal
+
+    return 0;
+
+}     
