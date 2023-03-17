3e81884dc8b2ac85c9451f54081bec7b65f68004
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-03-17 20:49:46 -0300

    indicando_o_numero_de_casas_ecimais_com_metodo_'precision'

--- printf_cout.cpp
+++ printf_cout.cpp
@@ -5,11 +5,17 @@
 using namespace std;
 int main() {
 
-    int num = 15;
+    //int num = 15
 
-    //printf("Valor de NUM %07d", num);       // %07d '07' indica o que estará sendo completado com seis 0 a esquerda
-    cout << "Valor de NUM em dec - " << num << "\n";
-    cout << "Valor de NUM em hexa - " << hex << num << "\n";      // 'hex' converte a variável em hexadeçimal
+    //printf("Valor de NUM %07d - ", num);       // %07d '07' indica o que estará sendo completado com seis 0 a esquerda
+    //cout << "Valor de NUM em dec - " << num << "\n";
+    //cout << "Valor de NUM em hexa - " << hex << num << "\n";      // 'hex' converte a variável em hexadeçimal
+
+    float pi = M_PI;
+
+    cout.precision(3);      // 'precision' método retorna a quantidade de casas decimais
+
+    cout << "Valor de PI " << pi << "\n";
 
     return 0;
 
