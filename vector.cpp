7b8149912e5ef48a7f5b2f59247fad548d2d6f67
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-03-18 16:54:13 -0300

    criando_novo_'vector'_ultilizando_metodo_'swap'_para_trocar_valores_dos_vectors

--- vector.cpp
+++ vector.cpp
@@ -4,20 +4,33 @@ using namespace std;
 
 int main() {
 
-    vector<int> num;        // declaração de 'vector'
+    vector<int> num1;        // declaração de 'vector'
+    vector<int> num2;
     int tam, i;     // declaração de variável 'tam' pra armazena o tamanho e 'i' que será o indice do 'for'
 
-    num.push_back(12);      // 'push_back' insere elementos no final de um 'vector'
-    num.push_back(9);
-    num.push_back(4);
-    num.push_back(6);
+    num1.push_back(1);      // 'push_back' insere elementos no final de um 'vector'
+    num1.push_back(2);
+    num1.push_back(3);
+    num1.push_back(4);
 
-    tam = num.size();        // método 'size' retorna o tamanho do 'vector' e armazena na variável 'tam'
+    num2.push_back(5);
+    num2.push_back(6);
+    num2.push_back(7);
+    num2.push_back(8);
+
+    num1.swap(num2);        // método 'swap' faz a troca de valores de dois 'vector'
+
+    tam = num1.size();        // método 'size' retorna o tamanho do vector 'num1' e armazena na variável 'tam'
 
     cout << "Tamanho do vector: " << tam << endl;
 
     for(i = 0; i < tam; i++) {      // imprimindo os elementos do 'vector' ultilizando 'for'
-        cout << num[i] << " ";
+        cout << num1[i] << " ";
+    }
+
+    cout << endl;        // 'endl' faz uma quebra de linha
+    for(i = 0; i < tam; i++) {
+        cout << num2[i] << " ";
     }
 
     return 0;
