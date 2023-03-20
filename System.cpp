cd3908a5092c4a2cbcbe697e9b594f2104c49c5e
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-03-19 22:01:10 -0300

    metodo_'system'_que_tem_a_funcao_de_chamar_comandos_do_sistema_operacional_'SO'_no_caso_do_windows_o_metodo_system_executa_comandos_do_prompt

---
+++ System.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main() {
+
+    system("color 74"); // comando 'color' troca a cor do prompt de comando
+    cout << "Luis Henrique Silva Ferreira";
+    //system("dir");       // 'system' recebe um parâmetro um comando do 'SO' comando 'dir' faz a listagem de diretorios
+    //system("cls");          // comando 'cls' limpa a tela
+    system("pause");            // sytem com comando 'pause' gera uma pausa até uma tecla for precionada
+
+    return 0;
+
+}
